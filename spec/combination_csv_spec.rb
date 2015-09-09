daffbbb1e0680d096bdc19643582982dd1ce81de
Junichi Ito <jit@sonicgarden.jp>
2015-09-09 23:30:47 +0900

    Reformat code

--- spec/combination_csv_spec.rb
+++ spec/combination_csv_spec.rb
@@ -32,15 +32,17 @@ describe CombinationCsv do
     let(:input_dir) { File.expand_path('../input', __FILE__) }
     let(:input_path) { File.join(input_dir, 'test2.csv') }
     let(:output_dir) { File.expand_path('../output', __FILE__) }
+
     def output_files
       Dir.glob(File.join(output_dir, '*.csv'))
     end
+
     before do
       FileUtils.rm(output_files)
     end
     example do
       expect {
-        CombinationCsv.generate_csv(input_path, output_dir, 6 ,3)
+        CombinationCsv.generate_csv(input_path, output_dir, 6, 3)
       }.to change { output_files.size }.from(0).to(28)
 
       output_path = File.join(output_dir, 'A1_600.csv')
@@ -54,44 +56,44 @@ describe CombinationCsv do
       output_path = File.join(output_dir, 'A1_510.csv')
       result = CSV.read(output_path)
       expect(result).to eq([
-                              %w(A1 5 test),
-                              %w(A2 1 test),
-                              %w(A3 0 test)
-      ])
+                               %w(A1 5 test),
+                               %w(A2 1 test),
+                               %w(A3 0 test)
+                           ])
     end
   end
 
   describe '::generate_combination' do
     let(:expected) do
       [
-          [6,0,0],
-          [5,1,0],
-          [5,0,1],
-          [4,2,0],
-          [4,0,2],
-          [4,1,1],
-          [3,3,0],
-          [3,0,3],
-          [3,2,1],
-          [3,1,2],
-          [2,4,0],
-          [2,0,4],
-          [2,3,1],
-          [2,1,3],
-          [2,2,2],
-          [1,5,0],
-          [1,0,5],
-          [1,4,1],
-          [1,1,4],
-          [1,3,2],
-          [1,2,3],
-          [0,6,0],
-          [0,0,6],
-          [0,5,1],
-          [0,1,5],
-          [0,4,2],
-          [0,2,4],
-          [0,3,3]
+          [6, 0, 0],
+          [5, 1, 0],
+          [5, 0, 1],
+          [4, 2, 0],
+          [4, 0, 2],
+          [4, 1, 1],
+          [3, 3, 0],
+          [3, 0, 3],
+          [3, 2, 1],
+          [3, 1, 2],
+          [2, 4, 0],
+          [2, 0, 4],
+          [2, 3, 1],
+          [2, 1, 3],
+          [2, 2, 2],
+          [1, 5, 0],
+          [1, 0, 5],
+          [1, 4, 1],
+          [1, 1, 4],
+          [1, 3, 2],
+          [1, 2, 3],
+          [0, 6, 0],
+          [0, 0, 6],
+          [0, 5, 1],
+          [0, 1, 5],
+          [0, 4, 2],
+          [0, 2, 4],
+          [0, 3, 3]
       ]
     end
     example do
